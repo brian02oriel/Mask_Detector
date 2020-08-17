f0e902d62cd0e1c058a7935daa98154412960f6c
brian02oriel <brian.nieto1@utp.ac.pa>
2020-08-17 11:19:09 -0500

    deleting cnn

--- training.py
+++ training.py
@@ -28,46 +28,4 @@ y = np.asarray(positive_labels + negative_labels)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 print(len(X_train), len(X_test), len(y_train), len(y_test))
 
-# Import Necessary Keras Libraries
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Flatten, Dropout
-from keras.layers import Conv2D, MaxPooling2D
 
-# Define model parameters
-num_classes = 2
-batch_size = 32
-
-# Build CNN model using Sequential API
-model = Sequential()
-
-# First layer group containing Convolution, Relu and MaxPooling layers
-model.add(Conv2D(64, (3,3), input_shape=(100, 100, 1)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# Second layer group containing Convolution, Relu and MaxPooling layers
-model.add(Conv2D(64, (3,3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-# Flatten and Dropout Layer to stack the output convolutions above as well as cater overfitting
-model.add(Flatten())
-model.add(Dropout(0.5))
-
-# Softmax Classifier
-model.add(Dense(64, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
-
-print(model.summary())
-
-# Plot the model
-from keras.utils.vis_utils import plot_model
-plot_model(model, to_file='face_mask_detection_architecture.png')
-
-# Train the model
-from keras.optimizers import Adam
-epochs = 50
-
-model.compile(loss= 'categorical_crossentropy', optimizer= Adam(lr=0.001), metrics= ['accuracy'])
-
-fitted_model = model.fit(np.asarray(X_train), np.asarray(y_train), epochs= epochs, validation_split= 0.25)
